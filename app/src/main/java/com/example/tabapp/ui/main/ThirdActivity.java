44af4ed9cc6c8e81820a34e9ce7ae3f01aaff9b6
sonic31rus <a400202@mail.ru>
2020-11-22 11:19:28 +0300

    First Create

---
+++ app/src/main/java/com/example/tabapp/ui/main/ThirdActivity.java
@@ -0,0 +1,23 @@
+package com.example.tabapp.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tabapp.R;
+
+public class ThirdActivity extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle sawedInstanceState)
+    {
+        return inflater.inflate(R.layout.third_main, container, false);
+    }
+
+}
